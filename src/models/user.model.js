6b8c82666947a8f19b87315b3e62f922639e848c
dakshs <dakshs112@gmail.com>
2025-08-02 14:23:26 +0530

    User and Video model added with jwt

---
+++ src/models/user.model.js
@@ -0,0 +1,91 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+            minlength: 3,
+            maxlength: 20,
+            match: /^[a-zA-Z0-9_]+$/,
+
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+        },
+        avatar: {
+            type: String,
+            required: true,
+            default: 'https://example.com/default-avatar.png',
+        },
+        coverImage: {
+            type: String,
+            required: true,
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Video',
+            }
+        ]
+    },
+    {
+        timestamps: true,
+    }
+)
+//To hash the password before saving the user 
+UserSchema.pre('save',async function(next){
+    if(!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password,10)
+    next();
+})
+UserSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+UserSchema.methods.generateAuthToken = function(){
+    return  jwt.sign(
+        {
+            _id : this._id,
+            username: this.username,
+            email: this.email,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+UserSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+export const User = moongose.model('User', UserSchema)
---
+++ src/models/video.model.js
@@ -0,0 +1,44 @@
+import moongoose,{Schema} from 'moongoose';
+var aggregatePaginate = require("mongoose-aggregate-paginate-v2");
+const VideoSchema = new Schema({
+    videoFile:{
+        type: String,
+        required:true,
+        unique: true,
+    },
+    thumbnail:{
+        type: String,
+        required: true,
+    },
+    title:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type: String,
+        required:true,
+    },
+    duration:{
+        type: Number,
+        required: true,
+    },
+    views:{
+        type: Number,
+        default: 0,
+    },
+    isPublished:{
+        type: Boolean,
+        default: true,
+    },
+    Owner:{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+
+    },
+},{
+    timestamps: true,
+    collection: 'videos',
+})
+VideoSchema.plugin(aggregatePaginate);
+export const Video = moongoose.model('Video',VideoSchema)
