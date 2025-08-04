8b880598af83bba007853e43926b6cb40a9d0cf5
dakshs <dakshs112@gmail.com>
2025-08-04 14:23:10 +0530

    Multer middleware and Cloduinary utlity included

---
+++ src/middleware/Multer.middleware.js
@@ -0,0 +1,28 @@
+const multer = require('multer');
+const storage = multer.diskStorageStorage({
+    destination: function (req, res, cb) {
+        cb(null, '/public/temp');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+
+});
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
+
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only .jpg, .jpeg, and .png files are allowed"), false);
+  }
+};
+
+
+export const upload = multer({
+
+    storage,
+    FileFilter,
+    
+}
+)
